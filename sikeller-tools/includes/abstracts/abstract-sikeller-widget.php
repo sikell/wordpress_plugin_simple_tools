2eab124df8d92846997a207b29363dccfa0129c5
Simon Keller <simonkeller94@aol.com>
2018-03-05 19:08:16 +0100

    Add number input in widget backend

--- sikeller-tools/includes/abstracts/abstract-sikeller-widget.php
+++ sikeller-tools/includes/abstracts/abstract-sikeller-widget.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Abstract Widget Class
  *
@@ -9,62 +10,64 @@
  * @category Widgets
  * @author   Simon Keller
  */
-abstract class SK_Widget extends WP_Widget {
+abstract class SK_Widget extends WP_Widget
+{
 
-	/**
-	 * CSS class.
-	 *
-	 * @var string
-	 */
-	public $widget_cssclass;
+    /**
+     * CSS class.
+     *
+     * @var string
+     */
+    public $widget_cssclass;
 
-	/**
-	 * Widget description.
-	 *
-	 * @var string
-	 */
-	public $widget_description;
+    /**
+     * Widget description.
+     *
+     * @var string
+     */
+    public $widget_description;
 
-	/**
-	 * Widget ID.
-	 *
-	 * @var string
-	 */
-	public $widget_id;
+    /**
+     * Widget ID.
+     *
+     * @var string
+     */
+    public $widget_id;
 
-	/**
-	 * Widget name.
-	 *
-	 * @var string
-	 */
-	public $widget_name;
+    /**
+     * Widget name.
+     *
+     * @var string
+     */
+    public $widget_name;
 
-	/**
-	 * Widget Settings.
-	 *
-	 * @var array
-	 */
-	public $settings = array();
+    /**
+     * Widget Settings.
+     *
+     * @var array
+     */
+    public $settings = array();
 
-	/**
-	 * Widget Control Options.
-	 *
-	 * @var array
-	 */
-	public $control_ops = array();
+    /**
+     * Widget Control Options.
+     *
+     * @var array
+     */
+    public $control_ops = array();
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct() {
-		$widget_ops = array(
-			'classname'   => $this->widget_cssclass,
-			'description' => $this->widget_description,
-			'customize_selective_refresh' => true,
-		);
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $widget_ops = array(
+            'classname' => $this->widget_cssclass,
+            'description' => $this->widget_description,
+            'customize_selective_refresh' => true,
+        );
 
-		parent::__construct( $this->widget_id, $this->widget_name, $widget_ops, $this->control_ops );
-	}
+        parent::__construct($this->widget_id, $this->widget_name, $widget_ops, $this->control_ops);
+    }
 
     /**
      * Output the html at the start of a widget.
@@ -73,10 +76,11 @@ abstract class SK_Widget extends WP_Widget {
      * @param  $instance
      * @return string
      */
-    public function widget_start( $args, $instance ) {
+    public function widget_start($args, $instance)
+    {
         echo $args['before_widget'];
 
-        if ( $title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base ) ) {
+        if ($title = apply_filters('widget_title', empty($instance['title']) ? '' : $instance['title'], $instance, $this->id_base)) {
             echo $args['before_title'] . $title . $args['after_title'];
         }
     }
@@ -87,7 +91,8 @@ abstract class SK_Widget extends WP_Widget {
      * @param  array $args
      * @return string
      */
-    public function widget_end( $args ) {
+    public function widget_end($args)
+    {
         echo $args['after_widget'];
     }
 
@@ -98,8 +103,9 @@ abstract class SK_Widget extends WP_Widget {
      * @see   WP_Widget->form
      * @param array $instance
      */
-    public function form( $instance ) {
-        if ( empty( $this->settings ) ) {
+    public function form($instance)
+    {
+        if (empty($this->settings)) {
             return;
         }
 
@@ -108,16 +114,17 @@ abstract class SK_Widget extends WP_Widget {
         <div class="sikeller-tab-title-container">
             <?php $group_count = 1;
 
-            foreach ( $this->settings as $key => $setting ) {
+            foreach ($this->settings as $key => $setting) {
 
-                $group_name = isset( $setting['group'] ) ? $setting['group'] : __( 'General', 'sikeller-tools' );
+                $group_name = isset($setting['group']) ? $setting['group'] : __('General', 'sikeller-tools');
 
-                if ( ! in_array( $group_name, $group_name_array )) {
+                if (!in_array($group_name, $group_name_array)) {
                     $group_name_array[] = $group_name;
 
-                    if( $group_name_array[0] != '' ) { ?>
-                        <a class="sikeller-tools-tab-title <?php echo ($group_count == 1 ? ' active' : '' ); ?>" href="#sikeller-tools-tab-<?php echo esc_attr( $group_count ); ?>">
-                            <?php echo esc_html( $group_name ); ?>
+                    if ($group_name_array[0] != '') { ?>
+                        <a class="sikeller-tools-tab-title <?php echo($group_count == 1 ? ' active' : ''); ?>"
+                           href="#sikeller-tools-tab-<?php echo esc_attr($group_count); ?>">
+                            <?php echo esc_html($group_name); ?>
                         </a>
                         <?php
                     }
@@ -129,22 +136,39 @@ abstract class SK_Widget extends WP_Widget {
         <div class="sikeller-tools-tab-content-container">
             <?php $group_count = 1;
 
-            foreach ( $group_name_array as $group ) { ?>
-                <div class="sikeller-tools-tab" id="sikeller-tools-tab-<?php echo esc_attr( $group_count ); ?>">
-                    <?php foreach ( $this->settings as $key => $setting ) {
-                        $current_setting_group = isset( $setting['group'] ) ? $setting['group'] : __( 'General', 'sikeller-tools' );
-                        if ( $current_setting_group == $group || empty( $group_name_array ) ) {
-                            $class = isset( $setting['class'] ) ? $setting['class'] : '';
-                            $value = isset( $instance[ $key ] ) ? $instance[ $key ] : $setting['std'];
-                            $field_width = isset( $setting['field_width'] ) ? $setting['field_width'] : 'col-full';
+            foreach ($group_name_array as $group) { ?>
+                <div class="sikeller-tools-tab" id="sikeller-tools-tab-<?php echo esc_attr($group_count); ?>">
+                    <?php foreach ($this->settings as $key => $setting) {
+                        $current_setting_group = isset($setting['group']) ? $setting['group'] : __('General', 'sikeller-tools');
+                        if ($current_setting_group == $group || empty($group_name_array)) {
+                            $class = isset($setting['class']) ? $setting['class'] : '';
+                            $value = isset($instance[$key]) ? $instance[$key] : $setting['std'];
+                            $field_width = isset($setting['field_width']) ? $setting['field_width'] : 'col-full';
 
-                            switch ( $setting['type'] ) {
+                            switch ($setting['type']) {
+                                case 'number' :
+                                    ?>
+                                    <p class="sk-widget-col <?php echo esc_attr($field_width); ?>">
+                                        <label for="<?php echo $this->get_field_id($key); ?>"><?php echo $setting['label']; ?></label>
+                                        <input class="widefat <?php echo esc_attr($class); ?>"
+                                               id="<?php echo esc_attr($this->get_field_id($key)); ?>"
+                                               name="<?php echo $this->get_field_name($key); ?>"
+                                               type="number" step="<?php echo esc_attr($setting['step']); ?>"
+                                               min="<?php echo esc_attr($setting['min']); ?>"
+                                               max="<?php echo esc_attr($setting['max']); ?>"
+                                               value="<?php echo esc_attr($value); ?>"/>
+                                    </p>
+                                    <?php
+                                    break;
 
                                 case 'text' :
                                     ?>
-                                    <p class="sk-widget-col <?php echo esc_attr( $field_width ); ?>">
-                                        <label for="<?php echo $this->get_field_id( $key ); ?>"><?php echo $setting['label']; ?></label>
-                                        <input class="widefat <?php echo esc_attr( $class ); ?>" id="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>" name="<?php echo $this->get_field_name( $key ); ?>" type="text" value="<?php echo esc_attr( $value ); ?>" />
+                                    <p class="sk-widget-col <?php echo esc_attr($field_width); ?>">
+                                        <label for="<?php echo $this->get_field_id($key); ?>"><?php echo $setting['label']; ?></label>
+                                        <input class="widefat <?php echo esc_attr($class); ?>"
+                                               id="<?php echo esc_attr($this->get_field_id($key)); ?>"
+                                               name="<?php echo $this->get_field_name($key); ?>" type="text"
+                                               value="<?php echo esc_attr($value); ?>"/>
                                     </p>
                                     <?php
                                     break;
@@ -152,10 +176,13 @@ abstract class SK_Widget extends WP_Widget {
                                 case 'textarea' :
                                     ?>
                                     <p>
-                                        <label for="<?php echo $this->get_field_id( $key ); ?>"><?php echo $setting['label']; ?></label>
-                                        <textarea class="widefat <?php echo esc_attr( $class ); ?>" id="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>" name="<?php echo $this->get_field_name( $key ); ?>" cols="20" rows="3"><?php echo esc_textarea( $value ); ?></textarea>
-                                        <?php if ( isset( $setting['desc'] ) ) : ?>
-                                            <small><?php echo esc_html( $setting['desc'] ); ?></small>
+                                        <label for="<?php echo $this->get_field_id($key); ?>"><?php echo $setting['label']; ?></label>
+                                        <textarea class="widefat <?php echo esc_attr($class); ?>"
+                                                  id="<?php echo esc_attr($this->get_field_id($key)); ?>"
+                                                  name="<?php echo $this->get_field_name($key); ?>" cols="20"
+                                                  rows="3"><?php echo esc_textarea($value); ?></textarea>
+                                        <?php if (isset($setting['desc'])) : ?>
+                                            <small><?php echo esc_html($setting['desc']); ?></small>
                                         <?php endif; ?>
                                     </p>
                                     <?php
@@ -163,11 +190,13 @@ abstract class SK_Widget extends WP_Widget {
 
                                 case 'select' :
                                     ?>
-                                    <p class="sk-widget-col <?php echo esc_attr( $field_width ); ?>">
-                                        <label for="<?php echo $this->get_field_id( $key ); ?>"><?php echo $setting['label']; ?></label>
-                                        <select class="widefat <?php echo esc_attr( $class ); ?>" id="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>" name="<?php echo $this->get_field_name( $key ); ?>">
-                                            <?php foreach ( $setting['options'] as $option_key => $option_value ) : ?>
-                                                <option value="<?php echo esc_attr( $option_key ); ?>" <?php selected( $option_key, $value ); ?>><?php echo esc_html( $option_value ); ?></option>
+                                    <p class="sk-widget-col <?php echo esc_attr($field_width); ?>">
+                                        <label for="<?php echo $this->get_field_id($key); ?>"><?php echo $setting['label']; ?></label>
+                                        <select class="widefat <?php echo esc_attr($class); ?>"
+                                                id="<?php echo esc_attr($this->get_field_id($key)); ?>"
+                                                name="<?php echo $this->get_field_name($key); ?>">
+                                            <?php foreach ($setting['options'] as $option_key => $option_value) : ?>
+                                                <option value="<?php echo esc_attr($option_key); ?>" <?php selected($option_key, $value); ?>><?php echo esc_html($option_value); ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                     </p>
@@ -176,29 +205,33 @@ abstract class SK_Widget extends WP_Widget {
 
                                 case 'image' :
                                     ?>
-                                    <div id="tg-widget-image-uploader" class="sikeller-media <?php echo esc_attr( $class ); ?>">
-                                        <p><label for="<?php echo $this->get_field_id( $key ); ?>"><?php echo esc_html( $setting['label'] ); ?></label></p>
-                                        <div class="media-uploader" id="<?php echo $this->get_field_id( $key ); ?>">
+                                    <div id="tg-widget-image-uploader"
+                                         class="sikeller-media <?php echo esc_attr($class); ?>">
+                                        <p>
+                                            <label for="<?php echo $this->get_field_id($key); ?>"><?php echo esc_html($setting['label']); ?></label>
+                                        </p>
+                                        <div class="media-uploader" id="<?php echo $this->get_field_id($key); ?>">
                                             <div class="tg-media-preview">
                                                 <button class="tg-media-remove dashicons dashicons-no-alt">
-                                                    <span class="screen-reader-text"><?php esc_html_e( 'Remove media', 'sikeller-tools' ) ?></span>
+                                                    <span class="screen-reader-text"><?php esc_html_e('Remove media', 'sikeller-tools') ?></span>
                                                 </button>
-                                                <?php if ( $value != '' ) : ?>
-                                                    <img class="tg-media-preview-default" src="<?php echo esc_url( $value ); ?>" />
+                                                <?php if ($value != '') : ?>
+                                                    <img class="tg-media-preview-default"
+                                                         src="<?php echo esc_url($value); ?>"/>
                                                 <?php endif; ?>
                                             </div>
                                             <p>
                                                 <input type="text" class="widefat tg-media-input"
-                                                       id="<?php echo $this->get_field_id( $key ); ?>"
-                                                       name="<?php echo $this->get_field_name( $key ); ?>"
-                                                       value="<?php echo esc_url( $value ); ?>"
-                                                       style="margin-top:5px;" />
+                                                       id="<?php echo $this->get_field_id($key); ?>"
+                                                       name="<?php echo $this->get_field_name($key); ?>"
+                                                       value="<?php echo esc_url($value); ?>"
+                                                       style="margin-top:5px;"/>
                                                 <button class="tg-image-upload button button-secondary button-large"
-                                                        id="<?php echo $this->get_field_id( $key ); ?>"
-                                                        data-choose="<?php esc_attr_e( 'Choose an image', 'sikeller-tools' ); ?>"
-                                                        data-update="<?php esc_attr_e( 'Use image', 'sikeller-tools' ); ?>"
+                                                        id="<?php echo $this->get_field_id($key); ?>"
+                                                        data-choose="<?php esc_attr_e('Choose an image', 'sikeller-tools'); ?>"
+                                                        data-update="<?php esc_attr_e('Use image', 'sikeller-tools'); ?>"
                                                         style="width:100%;margin-top:6px;margin-right:30px;">
-                                                    <?php esc_html_e( 'Select an Image', 'sikeller-tools' ); ?></button>
+                                                    <?php esc_html_e('Select an Image', 'sikeller-tools'); ?></button>
                                             </p>
                                         </div>
                                     </div>
@@ -207,7 +240,7 @@ abstract class SK_Widget extends WP_Widget {
 
                                 // Default: run an action.
                                 default :
-                                    do_action( 'sikeller_tools_toolkit_widget_field_' . $setting['type'], $key, $value, $setting, $instance );
+                                    do_action('sikeller_tools_toolkit_widget_field_' . $setting['type'], $key, $value, $setting, $instance);
                                     break;
                             } //End switch().
                         }
@@ -225,27 +258,28 @@ abstract class SK_Widget extends WP_Widget {
      * @param $old_instance
      * @return mixed
      */
-    public function update( $new_instance, $old_instance ) {
+    public function update($new_instance, $old_instance)
+    {
         $instance = $old_instance;
 
-        if ( empty( $this->settings ) ) {
+        if (empty($this->settings)) {
             return $instance;
         }
 
         // Loop settings and get values to save.
-        foreach ( $this->settings as $key => $setting ) {
+        foreach ($this->settings as $key => $setting) {
             if (!isset($setting['type'])) {
                 continue;
             }
 
             // Format the value based on settings type.
-            $instance[$key] = isset($new_instance[$key]) ? strip_tags( $new_instance[$key]) : '';
+            $instance[$key] = isset($new_instance[$key]) ? strip_tags($new_instance[$key]) : '';
 
             // Sanitize the value of a setting.
-            $instance[ $key ] = apply_filters( 'sikeller_tools_widget_settings_sanitize_option', $instance[ $key ], $new_instance, $key, $setting );
+            $instance[$key] = apply_filters('sikeller_tools_widget_settings_sanitize_option', $instance[$key], $new_instance, $key, $setting);
         }
 
         return $instance;
     }
-    
+
 }
